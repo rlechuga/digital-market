019402a432d52ad6a90f62e5356a396752067a8e
rlechuga <mail@rubenlechuga.com>
2024-02-15 21:44:48 -0800

    feat: stripe + user checkout session

--- src/collections/Products/Products.ts
+++ src/collections/Products/Products.ts
@@ -1,14 +1,57 @@
+// import { Product } from '@/payload-types';
 import { Access, CollectionConfig } from 'payload/types'
 import {
   AfterChangeHook,
   BeforeChangeHook,
 } from 'payload/dist/collections/config/types'
+import { Product, User } from '../../payload-types'
 
 import { PRODUCT_CATEGORIES } from '../../config'
+import { stripe } from '../../lib/stripe'
 
-// import { Product, User } from '../../payload-types'
+const addUser: BeforeChangeHook<Product> = async ({
+  req,
+  data,
+}) => {
+  const user = req.user
 
-// import { stripe } from '../../lib/stripe'
+  return { ...data, user: user.id }
+}
+
+// const syncUser: AfterChangeHook<Product> = async ({
+//   req,
+//   doc,
+// }) => {
+//   const fullUser = await req.payload.findByID({
+//     collection: 'users',
+//     id: req.user.id,
+//   })
+
+//   if (fullUser && typeof fullUser === 'object') {
+//     const { products } = fullUser
+
+//     const allIDs = [
+//       ...(products?.map((product) =>
+//         typeof product === 'object' ? product.id : product
+//       ) || []),
+//     ]
+
+//     const createdProductIDs = allIDs.filter(
+//       (id, index) => allIDs.indexOf(id) === index
+//     )
+
+//     const dataToUpdate = [...createdProductIDs, doc.id]
+
+//     await req.payload.update({
+//       collection: 'users',
+//       id: fullUser.id,
+//       data: {
+//         products: dataToUpdate,
+//       },
+//     })
+//   }
+// }
+
 
 export const Products: CollectionConfig = {
   slug: 'products',
@@ -20,6 +63,50 @@ export const Products: CollectionConfig = {
     // read: Access.userIsAdmin,
     // update: Access.userIsAdmin,
     // delete: Access.userIsAdmin,
+  },
+  hooks: {
+    // afterChange: [syncUser],
+    beforeChange: [
+      addUser,
+      async (args) => {
+        if (args.operation === 'create') {
+          const data = args.data as Product
+
+          const createdProduct =
+            await stripe.products.create({
+              name: data.name,
+              default_price_data: {
+                currency: 'USD',
+                unit_amount: Math.round(data.price * 100),
+              },
+            })
+
+          const updated: Product = {
+            ...data,
+            stripeId: createdProduct.id,
+            priceId: createdProduct.default_price as string,
+          }
+
+          return updated
+        } else if (args.operation === 'update') {
+          const data = args.data as Product
+
+          const updatedProduct =
+            await stripe.products.update(data.stripeId!, {
+              name: data.name,
+              default_price: data.priceId!,
+            })
+
+          const updated: Product = {
+            ...data,
+            stripeId: updatedProduct.id,
+            priceId: updatedProduct.default_price as string,
+          }
+
+          return updated
+        }
+      },
+    ],
   },
   fields: [
     {
