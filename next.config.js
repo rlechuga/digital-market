b1136e22d3b18a2cc4a1060659499b0ba05b523c
rlechuga <mail@rubenlechuga.com>
2024-02-22 01:08:12 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
 			},
 			{
 				protocol: "https",
-				hostname: "digitalmarket-production.up.railway.app",
+				hostname: "digital-market-mocha.vercel.app",
 			},
 		],
 	},
